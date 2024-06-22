0e0c68d50f4439c659c76e71b409a735b279c69a
Matthias Weiß <mail@matthiasweiss.at>
2024-06-22 14:38:46 +0200

    Refactor

--- lua/plugin/init.lua
+++ lua/plugin/init.lua
@@ -1,20 +1,14 @@
 local M = {} -- M stands for module, a naming convention
 
--- https://stackoverflow.com/questions/72921364/whats-the-lua-equivalent-of-pythons-endswith
-function string:endswith(suffix)
-    ---@diagnostic disable-next-line: param-type-mismatch
-    return self:sub(- #suffix) == suffix
-end
-
 function M.quick_switch_toggle()
     local relative_file_path = vim.fn.expand("%")
-    if relative_file_path:endswith ".component.ts" then
+    if string.match(relative_file_path, ".component.ts$") then
         local file_to_open = relative_file_path:gsub(".ts$", ".html")
         vim.cmd.edit(file_to_open)
         return
     end
 
-    if relative_file_path:endswith ".component.html" then
+    if string.match(relative_file_path, ".component.html$") then
         local file_to_open = relative_file_path:gsub(".html$", ".ts")
         vim.cmd.edit(file_to_open)
         return
