334459dd37ffe04fbd74639ef86101fc58045d77
Matthias Weiß <mail@matthiasweiss.at>
2024-09-21 15:17:30 +0200

    Add NgQuickSwitchComponent command

--- lua/plugin/init.lua
+++ lua/plugin/init.lua
@@ -1,23 +1,20 @@
 local M = {}
 
 local file_path_transformations = {
+    unchanged = function(path) return path end,
     componentToTemplate = function(path) return path:gsub("%.ts$", ".html") end,
     templateToComponent = function(path) return path:gsub("%.html$", ".ts") end,
     testToClass = function(path) return path:gsub("%.spec%.ts$", ".ts") end,
     classToTest = function(path) return path:gsub("%.ts$", ".spec.ts") end,
+    cssToClass = function(path) return path:gsub("%.css$", ".ts") end,
+    scssToClass = function(path) return path:gsub("%.scss$", ".ts") end,
+    sassToClass = function(path) return path:gsub("%.sass$", ".ts") end,
 }
 
-function M.quick_switch_toggle()
+local function open_target_file(params)
     local relative_file_path = vim.fn.expand("%")
 
-    local file_path_transformation_map = {
-        { regex = "%.component%.ts$",   transform = file_path_transformations.componentToTemplate },
-        { regex = "%.component%.html$", transform = file_path_transformations.templateToComponent },
-        { regex = "%.spec%.ts$",        transform = file_path_transformations.testToClass },
-        { regex = "%.ts$",              transform = file_path_transformations.classToTest },
-    }
-
-    for _, transformation in ipairs(file_path_transformation_map) do
+    for _, transformation in ipairs(params.file_path_transformation_map) do
         if string.match(relative_file_path, transformation.regex) then
             local file_to_open = transformation.transform(relative_file_path)
             vim.cmd.edit(file_to_open)
@@ -25,13 +22,38 @@ function M.quick_switch_toggle()
         end
     end
 
-    error(":NgQuickSwitchToggle could not determine file that should be opened", 1)
+    error(params.command .. " could not determine target file", 1)
+end
+
+function M.quick_switch_toggle()
+    local file_path_transformation_map = {
+        { regex = "%.component%.ts$",   transform = file_path_transformations.componentToTemplate },
+        { regex = "%.component%.html$", transform = file_path_transformations.templateToComponent },
+        { regex = "%.spec%.ts$",        transform = file_path_transformations.testToClass },
+        { regex = "%.ts$",              transform = file_path_transformations.classToTest },
+    }
+
+    open_target_file({ file_path_transformation_map = file_path_transformation_map, command = "NgQuickSwitchToggle" })
+end
+
+function M.quick_switch_component()
+    local file_path_transformation_map = {
+        { regex = "%.component%.ts$",       transform = file_path_transformations.unchanged },
+        { regex = "%.component%.html$",     transform = file_path_transformations.templateToComponent },
+        { regex = "%.component%.spec%.ts$", transform = file_path_transformations.testToClass },
+        { regex = "%.component%.css$",      transform = file_path_transformations.cssToClass },
+        { regex = "%.component%.scss$",     transform = file_path_transformations.scssToClass },
+        { regex = "%.component%.sass$",     transform = file_path_transformations.sassToClass },
+    }
+
+    open_target_file({ file_path_transformation_map = file_path_transformation_map, command = "NgQuickSwitchComponent" })
 end
 
 function M.setup(opts)
     opts = opts or {}
 
     vim.api.nvim_create_user_command("NgQuickSwitchToggle", M.quick_switch_toggle, {})
+    vim.api.nvim_create_user_command("NgQuickSwitchComponent", M.quick_switch_component, {})
 
     if opts.use_default_keymaps then
         vim.keymap.set("n", "<leader>qs", ":NgQuickSwitchToggle<cr>")
