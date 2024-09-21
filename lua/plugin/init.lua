15e285d2f8b01acbb76e7ae61d62bb21e640aa4d
Matthias Weiß <mail@matthiasweiss.at>
2024-09-21 14:51:01 +0200

    Add missing returns

--- lua/plugin/init.lua
+++ lua/plugin/init.lua
@@ -1,10 +1,10 @@
 local M = {}
 
 local file_path_transformations = {
-    componentToTemplate = function(path) path:gsub("%.ts$", ".html") end,
-    templateToComponent = function(path) path:gsub("%.html$", ".ts") end,
-    testToClass = function(path) path:gsub("%.spec%.ts$", ".ts") end,
-    classToTest = function(path) path:gsub("%.ts$", ".spec.ts") end,
+    componentToTemplate = function(path) return path:gsub("%.ts$", ".html") end,
+    templateToComponent = function(path) return path:gsub("%.html$", ".ts") end,
+    testToClass = function(path) return path:gsub("%.spec%.ts$", ".ts") end,
+    classToTest = function(path) return path:gsub("%.ts$", ".spec.ts") end,
 }
 
 function M.quick_switch_toggle()
@@ -19,6 +19,7 @@ function M.quick_switch_toggle()
 
     for _, transformation in ipairs(file_path_transformation_map) do
         if string.match(relative_file_path, transformation.regex) then
+            print(relative_file_path)
             local file_to_open = transformation.transform(relative_file_path)
             print(file_to_open)
             vim.cmd.edit(file_to_open)
